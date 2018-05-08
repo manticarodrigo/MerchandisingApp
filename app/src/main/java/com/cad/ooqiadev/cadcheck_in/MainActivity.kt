a0a80fcccfd069bd7d78f0d3bfd8243e91e47c61
manticarodrigo <manticarodrigo@gmail.com>
2018-05-08 08:09:13 -0600

    UI updates

--- app/src/main/java/com/cad/ooqiadev/cadcheck_in/MainActivity.kt
+++ app/src/main/java/com/cad/ooqiadev/cadcheck_in/MainActivity.kt
@@ -20,6 +20,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        // Set toolbar title
+        supportActionBar?.title = "Pendientes"
+
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
